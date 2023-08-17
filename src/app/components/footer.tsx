19d58fd04f5174b228ccf699fa4f61a4ed3e9986
DENNISMATENGE <mateshdennis@gmail.com>
2023-08-17 11:34:57 +0300

    first commit

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-5 items-center justify-center flex bg-blue-500 text-white h-10'>
+      <p>Copyright © 2023 Dennison. All rights reserved.</p>
+      <p className='hidden md:block'>This website is made with ❤️️ by Dennison.</p>
+      <small className='hidden md:block'>Privacy Policy | Terms of Service</small>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-green-500 text-white font-bold items-center h-10 justify-center uppercase  flex'>
+      Welcome to Harry Potters site. 
+    </div>
+  )
+}
+
+export default Navbar;
